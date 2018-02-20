f4dc5a8a2b35489a2f228c93700487459330eb29
Philippe Aucazou <paucazou@yahoo.fr>
2018-02-20 16:20:59 +0100

    An useless example in json

---
+++ frumul/headers/json.h
@@ -0,0 +1,8 @@
+lang "json" "o(object) a(array) p(pair) t(true) f(false) n(null)"
+(object : mark "2" "\{{}}"
+ array : mark "2" "[{}]"
+ pair : mark "3" "\"{}\": {}"
+ true : "true"
+ false : "false"
+ null : "null") //pretty useless, no ? Just an example.
+
