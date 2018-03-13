9461f9ecf107f29811c0d8b1904765c61277aa12
Philippe Aucazou <paucazou@yahoo.fr>
2018-03-13 14:48:20 +0100

    Fixed bug in PWD

--- dev.sh
+++ dev.sh
@@ -1,2 +1,11 @@
 # $ source dev.sh
+o_path=$PWD
+f_path=`dirname $0`
+cd $f_path
+f_path=$PWD
+
 export FRUMULTEST=0 # enable traceback for development purpose
+source ./frumurvenv/bin/activate
+alias frumul="$f_path/frumul.py"
+alias indexer="$f_path/indexer.py"
+cd $o_path
